b670efe3588ac8b68d2e65af5dcd4a3a7ee11de3
Julien Vanier <jvanier@gmail.com>
2015-01-18 13:07:23 -0500

    Minify and hash assets

--- config.rb
+++ config.rb
@@ -33,7 +33,7 @@
 ###
 
 # Automatic image dimensions on image_tag helper
-activate :automatic_image_sizes
+# activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
 configure :development do
@@ -56,13 +56,13 @@ set :images_dir, 'img'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
